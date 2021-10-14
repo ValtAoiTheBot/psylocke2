b2c1b1de74fad71692ad9c7b7a8b4c071626e222
Magneto <90975856+123creation@users.noreply.github.com>
2021-10-14 19:36:15 +0530

    Update __main__.py

--- SophiaBot/__main__.py
+++ SophiaBot/__main__.py
@@ -221,7 +221,8 @@ def start(update: Update, context: CallbackContext):
                 timeout=60,
             )
     else:
-        update.effective_message.reply_text("Heya, Sophia here :) PM me if you have any questions how to use me!")
+        update.effective_message.reply_text("Heya, Psylocke here 💖 I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+                uptime ) PM me if you have any questions how to use me!")
 
 
 def error_handler(update, context):
