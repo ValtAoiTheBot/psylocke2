938837b99faa7abfb89134817ecc5cf2d7a08a6b
Magneto <90975856+123creation@users.noreply.github.com>
2021-10-16 11:15:58 +0530

    Update welcome.py

--- SophiaBot/modules/welcome.py
+++ SophiaBot/modules/welcome.py
@@ -187,9 +187,12 @@ def new_member(update: Update, context: CallbackContext):
                     pass
                 reply = False
 
+   PSYLOCKE_IMG = "https://telegra.ph/file/a24ce8d67bba862d457a0.jpg"
+
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_text(
+                update.effective_message.reply_photo(
+            EMILIA_IMG, caption =
                     "Oh, Genos? Let's get this moving.", reply_to_message_id=reply
                 )
                 welcome_log = (
