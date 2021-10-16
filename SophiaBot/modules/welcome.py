fd8c473a859e04d2ebd75e3ee39ae2f5df4c8c66
Magneto <90975856+123creation@users.noreply.github.com>
2021-10-16 14:13:17 +0530

    Update welcome.py

--- SophiaBot/modules/welcome.py
+++ SophiaBot/modules/welcome.py
@@ -187,12 +187,10 @@ def new_member(update: Update, context: CallbackContext):
                     pass
                 reply = False
 
-   PSYLOCKE_IMG = "https://telegra.ph/file/a24ce8d67bba862d457a0.jpg"
-
-            # Give the owner a special welcome
+   # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_photo(
-            PSYLOCKE_IMG,caption = "Oh, Genos? Let's get this moving.", reply_to_message_id=reply
+                update.effective_message.reply_text( 
+           "Oh, Genos? Let's get this moving.", reply_to_message_id=reply
                 )
                 welcome_log = (
                     f"{html.escape(chat.title)}\n"
