b4291865d70ecbbcb3edc1e3f9f51000437e7248
Magneto <90975856+123creation@users.noreply.github.com>
2021-10-17 16:53:29 +0530

    Add files via upload

---
+++ SophiaBot/modules/gbam_strings.py
@@ -0,0 +1,33 @@
+GBUN = (
+    "Beware! This Bot-Admeme Can Gbun You Right Off The Map.",
+    "I Guess You've Forgot Spelling Of GBAN Maybe...?", 
+    "Don't Misuse Your Powers...",
+    "Nah, He Looks Innocent..."
+)
+
+
+GBAM = "<b>Beginning Of Global Bam For {user2}</b>  \n \nChat Id : <code>{chatid}</code> \nReason : <i>{reason}</i> \nGBammed By {user1}"                                                       
+
+
+GBAM_REASON = (
+    "Baka noob",
+    "Baka waifu stealer",
+    "Baka white-het hekur",
+    "Baka white-het codur",
+    "Baka madan op fan",
+    "Baka serial watcher",
+    "Baka chotta bheem fan",
+    "sasta tiktokur💃🏾",
+    "sasta membor of team 7",
+    "Baka vadakan",
+    "sasta fri-fire player",
+    "sasta chhota bhim",
+    "Baka baka",
+    "Baka boomer",
+    "Baka nibba",
+    "Baka niggee",
+    "Baka geh",
+    "Baka spy",
+    "Baka vadakan",
+
+)
