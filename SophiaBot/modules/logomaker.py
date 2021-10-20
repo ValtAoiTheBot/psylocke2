ec08c736e054b69c0ec14660219f32484c788593
Kwannon <90975856+123creation@users.noreply.github.com>
2021-10-20 10:41:41 +0530

    Update logomaker.py

--- SophiaBot/modules/logomaker.py
+++ SophiaBot/modules/logomaker.py
@@ -268,7 +268,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "black"
     shadowcolor = "blue"
-    fnt = glob.glob("./SophiaBot/resources/Streamster.ttf")
+    fnt = glob.glob("./SophiaBot/resources/Vampire Wars.otf")
     randf = random.choice(fnt)
     font = ImageFont.truetype(randf, 120)
     w, h = draw.textsize(text, font=font)
