728cfc1ed213abbdac76b0f95c60a64b0bd07bba
Kwannon <90975856+123creation@users.noreply.github.com>
2021-10-25 21:06:36 +0530

    Update blacklist_stickers.py

--- SophiaBot/modules/blacklist_stickers.py
+++ SophiaBot/modules/blacklist_stickers.py
@@ -494,7 +494,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "B-Sticker"
+__mod_name__ = "🚫 B-Sticker"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
