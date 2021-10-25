da71b7e54f793137dbaed57997695e15d8616699
Kwannon <90975856+123creation@users.noreply.github.com>
2021-10-25 14:10:34 +0530

    Update myinfo.py

--- SophiaBot/modules/myinfo.py
+++ SophiaBot/modules/myinfo.py
@@ -1,7 +1,11 @@
 import System
 from SophiaBot import events
-from telethon import Button
-await event.client.bot.send_message(event.chat_id, f'**➢ Hᴇʏ {(event.sender.first_name)}**\n\n**➢ I Aᴍ [Yᴜᴋɪɴᴀ](t.me/YuukiKonnoRobot)**\n**➢ I Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ [Sᴀɪ](t.me/Me_Iz_Mad_Boi)**', file='https://telegra.ph/file/a340604e274705673b96a.jpg', Button=[Button.inline('**Iɴғᴏ**', data="psylocke"), Button.url('**Sᴜᴘᴘᴏʀᴛ**', 'https://t.me/YuukiSupportChat')])
-@System.bot.on(events.CallbackQuery(pattern=r"psylocke"))
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+await event.client.bot.send_message(event.chat_id, f'**➢ Hᴇʏ {(event.sender.first_name)}**\n\n**➢ I Aᴍ [Yᴜᴋɪɴᴀ](t.me/YuukiKonnoRobot)**\n**➢ I Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ [Sᴀɪ](t.me/Me_Iz_Mad_Boi)**', file='https://telegra.ph/file/a340604e274705673b96a.jpg', 
+
+[
+        InlineKeyboardButton(text="➕ Aᴅᴅ Mᴇ", data="psylocke"),   
+    ],
+],Button@System.bot.on(events.CallbackQuery(pattern=r"psylocke"))
 async def ok(event):
      await event.answer(f'➢ Fɪʀsᴛ Nᴀᴍᴇ : {(event.sender.first_name)}\n➢ Lᴀsᴛ Nᴀᴍᴇ : {(event.sender.last_name)}\n➢ Usᴇʀɴᴀᴍᴇ : {(event.sender.username)}\n➢ Usᴇʀ Iᴅ : {(event.sender.id)}', alert=True)
