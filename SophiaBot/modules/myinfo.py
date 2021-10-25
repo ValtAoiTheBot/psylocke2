06e4598a4b3806787f32f178e6b15cc1e33c1516
Kwannon <90975856+123creation@users.noreply.github.com>
2021-10-25 13:54:37 +0530

    Update myinfo.py

--- SophiaBot/modules/myinfo.py
+++ SophiaBot/modules/myinfo.py
@@ -1,4 +1,4 @@
-from SophiaBot import System
+import System
 from telethon.sync import events
 from telethon import Button
 await event.client.bot.send_message(event.chat_id, f'**➢ Hᴇʏ {(event.sender.first_name)}**\n\n**➢ I Aᴍ [Yᴜᴋɪɴᴀ](t.me/YuukiKonnoRobot)**\n**➢ I Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ [Sᴀɪ](t.me/Me_Iz_Mad_Boi)**', file='https://telegra.ph/file/a340604e274705673b96a.jpg', buttons=[Button.inline('**Iɴғᴏ**', data="psylocke"), Button.url('**Sᴜᴘᴘᴏʀᴛ**', 'https://t.me/YuukiSupportChat')])
