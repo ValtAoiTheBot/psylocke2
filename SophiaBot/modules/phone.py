b56843193b94139a17711c59d72125b089e84c47
Kwannon <90975856+123creation@users.noreply.github.com>
2021-10-25 10:48:44 +0530

    Update phone.py

--- SophiaBot/modules/phone.py
+++ SophiaBot/modules/phone.py
@@ -1,28 +1,5 @@
 import json
 
-# Copyright (C) 2021 Dihan Official
-
-
-# This file is part of Sophia (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
-
-
-
 import requests
 
 from SophiaBot import telethn as client
@@ -92,7 +69,7 @@ async def phone(event):
 __help__ = """
 *Commands:*
 • `/phone <no>`*:* find phone number details.
-@SophiaSLBot
+@Psylocke_robot
 """
 
-__mod_name__ = "Phone"
+__mod_name__ = "📱 Phone"
