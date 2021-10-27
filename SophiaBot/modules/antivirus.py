2b6227b4bf8f82402f0a25ddb8fb969015b7d69d
Kwannon <90975856+123creation@users.noreply.github.com>
2021-10-27 11:28:23 +0530

    Update antivirus.py

--- SophiaBot/modules/antivirus.py
+++ SophiaBot/modules/antivirus.py
@@ -132,7 +132,7 @@ file_helpo = file_help.replace("_", " ")
 __help__ = """
  - /scanit: Scan a file for virus (MAX SIZE = 3MB)
 
- *Powerted by* @SophiaUpdates ❤
+ *Powerted by* @PigasusUpdates ❤
 """
 
-__mod_name__ = "🦠 Virus Scan"
+__mod_name__ = "🧫 Virus Scan"
