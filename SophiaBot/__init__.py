612466193ade95145ed0aeda7fdda4b2e1b6d11b
Kwannon <90975856+123creation@users.noreply.github.com>
2021-10-27 09:05:24 +0530

    Update __init__.py

--- SophiaBot/__init__.py
+++ SophiaBot/__init__.py
@@ -23,7 +23,7 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-# if version < 7.9, stop bot.
+# if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
         "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
